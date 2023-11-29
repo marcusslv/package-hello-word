d3a1aa2f21758c5b9ee0eda69319e2844cbebd4e
Marcus Vinicius da Silva <marcusjapaslv@hotmail.com>
2023-11-29 13:52:57 -0300

    :sparkles: Adicionando provider

---
+++ src/HelloWordBaseServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Package\HelloWord;
+
+use \Illuminate\Support\ServiceProvider;
+
+class HelloWordBaseServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
